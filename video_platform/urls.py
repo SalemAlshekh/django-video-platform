678d8b7dfe939de339a498f20ffb8540531fabac
SalemAlshekh <salemalshekh003@gmail.com>
2025-12-26 02:44:17 +0300

    Fix auth, signup form, media, access control

--- video_platform/urls.py
+++ video_platform/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 
 urlpatterns = [
 
@@ -14,6 +15,13 @@ urlpatterns = [
     # التطبيق الرئيسي (الفيديوهات)
     path('', include('videos.urls')),
 ]
+def maintenance_view(request):
+    return render(request, 'maintenance.html')
+
+if settings.MAINTENANCE_MODE:
+    urlpatterns = [
+        path('', maintenance_view),
+    ]
 
 # عرض ملفات media أثناء التطوير
 if settings.DEBUG:
